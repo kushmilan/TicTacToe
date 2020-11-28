e4bc92d2639f302247d20c8580307172e83ed54b
Milan Singh Kushwah <33677132+kushmilan@users.noreply.github.com>
2020-11-28 09:20:56 +0530

    Update ValidPosition.java

--- TicTacToe/src/tictactoe/ValidPosition.java
+++ TicTacToe/src/tictactoe/ValidPosition.java
@@ -11,14 +11,15 @@ import java.io.IOException;
 class ValidPosition {
     public int validPosition(Set<Integer> positions, BufferedReader br) throws IOException{
         int pos = 0;
-        while (!positions.contains(pos)) {
+        while (!positions.contains(pos)) { // For taking only integer as input
             String temp = br.readLine();
             while(!temp.chars().allMatch( Character::isDigit )){
                 System.out.println("Please enter a valid integer number!");
                 temp = br.readLine();
             }
             pos = Integer.parseInt(temp);
-            if (!positions.contains(pos)) {
+            
+            if (!positions.contains(pos)) { // For handling valid input from user
                 if(pos > 9 || pos < 1){
                     System.out.println("Please choose a valid position, position " + pos + " is out of range.");
                 }else{
